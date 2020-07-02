a56b1d3828218ff0ec001cf646afb4ef0c12512c
Aakash Goel <goelaakash79@gmail.com>
2020-07-02 18:05:50 +0530

    --init: basic str setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="flex flex-wrap justify-center items-center h-screen bg-gray-900">
+			<h1 className="font-serif md:text-4xl lg:text-4xl text-xl italic font-bold text-white md:h-10 lg:h-10 h-6 border-b-8 border-blue-400">
+				<code>boilerplate cra-tailwind</code>
+			</h1>
+		</div>
+	);
 }
 
 export default App;
